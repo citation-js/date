71a1404b81c079d590aa9f6187fe92ff74728c8e
Lars Willighagen <lars.willighagen@gmail.com>
2019-01-04 15:01:18 +0100

    test: test formatter

---
+++ test/output.spec.js
@@ -0,0 +1,19 @@
+/* eslint-env mocha */
+
+import assert from 'assert'
+import { format } from '../src/'
+
+describe('formatter', function () {
+  it('with nothing', function () {
+    assert.strictEqual(format({'date-parts': [[2000, 10, 10]]}), '2000-10-10')
+  })
+  it('with narrow parts', function () {
+    assert.strictEqual(format({'date-parts': [[1, 1, 1]]}), '0001-01-01')
+  })
+  it('with delimiter', function () {
+    assert.strictEqual(format({'date-parts': [[2000, 10, 10]]}, '='), '2000=10=10')
+  })
+  it('with raw date', function () {
+    assert.strictEqual(format({'raw': 'foo'}), 'foo')
+  })
+})
